a07347651182a6be318636702c57929876d27fa1
Tologon Omurzakov <bigbluelamp@gmail.com>
2021-11-11 22:36:32 +0600

    feat: create admin-panel app

--- interview_task_polls/settings.py
+++ interview_task_polls/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # local apps
+    'admin_panel.apps.AdminPanelConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
--- interview_task_polls/urls.py
+++ interview_task_polls/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('api/v1/admin-panel/', include('admin_panel.urls')),
     path('admin/', admin.site.urls),
 ]
