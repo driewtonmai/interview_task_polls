6d87e2893e4ba872e8b7c03ee7ca90205f151dea
Tologon Omurzakov <bigbluelamp@gmail.com>
2021-11-14 23:17:22 +0600

    feat: add urls for new API's

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,11 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.ListActivePollsView.as_view(), name='polls-active-list')
+    path('', views.ActivePollListView.as_view(), name='polls-active-list'),
+    path('<int:pk>/', views.ActivePollDetailView.as_view(), name='polls-detail'),
+    path('select-poll/', views.CreateUserSelectPollView.as_view(), name='select-poll'),
+    path('create-answer/', views.CreateAnswerView.as_view(), name='write-answer'),
+    path('client/<int:pk>/results/', views.ListClientResultsView.as_view(), name='list-client-results'),
+    path('client/<int:client_pk>/results/<int:selected_poll_pk>/',
+         views.RetrieveClientResultsView.as_view(), name='retrieve-client-results'),
 ]
