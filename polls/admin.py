6602006893fe6934285ac0438af1b70bbc400ed6
Tologon Omurzakov <bigbluelamp@gmail.com>
2021-11-14 23:16:17 +0600

    feat: registration of new models in the admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Poll, Question, QUESTION_TYPES, AnswerOptions, Answer, OptionChoices
+from .models import Poll, Question, Answer, OptionChoices, UserSelectedPoll, AnswerOptions
 
 
 admin.site.register(Poll)
 admin.site.register(Question)
-admin.site.register(AnswerOptions)
 admin.site.register(Answer)
 admin.site.register(OptionChoices)
+admin.site.register(UserSelectedPoll)
+admin.site.register(AnswerOptions)
